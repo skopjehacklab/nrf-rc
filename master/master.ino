4ce710fc21d1523079260640ecbb26048e81aa35
Aleks <aleksandar.lazarov1@gmail.com>
2013-12-18 16:23:51 +0100

    Cleaned up the code and added some info printf()'s

--- master/master.ino
+++ master/master.ino
@@ -9,32 +9,29 @@ String input;
 // Set up nRF24L01 radio on SPI bus plus pins 9 & 10
 RF24 radio(9,10);
 
-// Single radio pipe address for the 2 nodes to communicate.
+// This is the TX pipe. The slaves will listen on a address which is with an offset of 'address' to this one
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 
 int state = 0;
 uint8_t state_size = sizeof(state);
 
-void setup(void)
-{
+void setup(void) {
   Serial.begin(9600);
   printf_begin();
 
   radio.begin();
-
-  // This simple sketch opens a single pipes for these two nodes to communicate
-  // back and forth.  One listens on it, the other talks to it.
-
   radio.openWritingPipe(pipe);
-
+  printf("Booting up ....\n\r");
   radio.printDetails();
+  printf("\n\rBooted!\n\r");
+  printf("Syntax : '_addr_ _state_'\n\n\r");
 }
 
-void loop(void)
-{
-  int address;
+void loop(void) {
+  uint8_t address;
   char cmd[10];
   char c;
+
 
   while ( Serial.available() ) {
     c = Serial.read();
@@ -42,21 +39,22 @@ void loop(void)
       input += c;
     } else {
       input.toCharArray(cmd,10);
-      byte okparse = sscanf(cmd, "%d %d", &address, &state);
-      printf("%d", state);
+      byte okparse = sscanf(cmd, "%hhd %hhd", &address, &state);
+      //printf("%d", state);
 
       if (okparse == 2 ) {
         radio.openWritingPipe(pipe+(uint64_t)address);
 
-        radio.printDetails();
+        //radio.printDetails();
 
-        printf("Now sending...");
+        printf("Now sending...\n\r");
+        printf("Address : %hhd \n\rState : %hhd\n\r", address, state);
         bool okwrite = radio.write( &state, state_size );
-    
+
         if (okwrite)
-          printf("ok\n\r");
+          printf("OK\n\r");
         else
-          printf("failed\n\r");
+          printf("Failed\n\r");
       }
       input = "";
     }
