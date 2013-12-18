da5acc9fbf706511d5a5376ce66863373142309c
Aleks <aleksandar.lazarov1@gmail.com>
2013-12-18 16:50:25 +0100

    Added some info printf()'s

--- slaves/slaves.ino
+++ slaves/slaves.ino
@@ -3,39 +3,33 @@
 #include "RF24.h"
 #include "printf.h"
 
+#define ADDR_OFFSET 3 // Listeningt address offset
+
+
 RF24 radio(9,10);
 
-// Single radio pipe address for the 2 nodes to communicate.
+// The base of the listening address. The real one will have an offset of ADDR_OFFSET
 const uint64_t pipe = 0xE8E8F0F0E1LL;
-
-//
-// Payload
-//
 
 uint8_t state;
 uint8_t state_size = sizeof(state);
 
-void setup(void)
-{
+void setup(void) {
   Serial.begin(9600);
   printf_begin();
 
+  printf("Booting up...\n\r");
   radio.begin();
 
-  // This simple sketch opens a single pipes for these two nodes to communicate
-  // back and forth.  One listens on it, the other talks to it.
-  radio.openReadingPipe(1,pipe+3);
+  radio.openReadingPipe(1,pipe+ADDR_OFFSET);
   radio.startListening();
 
   radio.printDetails();
+  printf("Booted !\n\r");
 }
 
-//
-// Loop
-//
 
-void loop(void)
-{
+void loop(void) {
   // if there is data ready
   if ( radio.available() ) {
     // Dump the payloads until we've gotten everything
@@ -44,9 +38,8 @@ void loop(void)
 
       // Fetch the payload, and see if this was the last one.
       done = radio.read( &state, state_size );
-      
       // Spew it
-      printf("%d",state);
+      printf("Got state : %hhd\n\r", state);
     }
     printf("\r\n");
   }
